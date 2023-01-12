716f98e2d0c695e43c94b98ba7483275012691b6
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-12 12:12:31 +0530

    added: gameboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { GameBoard } from "./components";
+
 function App() {
   return (
-    <div className="flex items-center flex-col justify-center h-screen">
-      <h1 className="text-3xl">Hello World</h1>
-      <h1>This is where the magic happens</h1>
+    <div className="h-screen">
+      <div className="bg-white w-full h-full flex items-center justify-center">
+        <GameBoard />
+      </div>
     </div>
   );
 }
---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,7 @@
+const GameBoard = () => {
+  return (
+    <div className="border border-black w-[40%] h-[70%] grid grid-cols-30 grid-rows-20"></div>
+  );
+};
+
+export default GameBoard;
