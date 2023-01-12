247cf8787f1457b1d0848eeeaf5e4437e8501b58
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-12 12:05:40 +0530

    added custom row and cols numbers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        30: "repeat(30, 1fr)",
+      },
+      gridTemplateRows: {
+        20: "repeat(20, 1fr)",
+      },
+    },
   },
   plugins: [],
 };
