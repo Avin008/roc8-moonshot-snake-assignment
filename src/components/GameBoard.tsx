f2505c4d70cef27b6330a611cb82dc25c4f6e2f1
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-12 12:36:09 +0530

    added: gridCol state

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,4 +1,11 @@
+import { useEffect, useState } from "react";
+
 const GameBoard = () => {
+  const [grid, setGrid] = useState<{
+    colNum: number;
+    rowNum: number;
+  }>({ colNum: 1, rowNum: 1 });
+
   return (
     <div className="border border-black w-[40%] h-[70%] grid grid-cols-30 grid-rows-20"></div>
   );
