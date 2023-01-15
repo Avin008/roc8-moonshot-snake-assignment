6e0d889ec130b2ae42e5c78d56f74e48680325d0
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-15 22:25:17 +0530

    style: added dark mode styles

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -6,7 +6,7 @@ const Scores = ({
   topScore: number;
 }) => {
   return (
-    <div className="flex justify-between items-center text-white px-2 bg-slate-800 -top-9 h-8 absolute left-0 right-0">
+    <div className="flex justify-between items-center dark:text-white px-2 dark:bg-slate-800 -top-9 h-8 absolute left-0 right-0">
       <div>Your Score: {playerScore}</div>
       <div>Top Score: {topScore}</div>
     </div>
