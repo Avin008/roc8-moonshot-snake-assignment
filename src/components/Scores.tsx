027cf74377ff4b023b251d34c8f92f395a0a0808
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-15 22:52:02 +0530

    style: added leaderboard icon

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -1,3 +1,4 @@
+import { AiFillTrophy } from "react-icons/ai";
 const Scores = ({
   playerScore,
   topScore,
@@ -8,7 +9,13 @@ const Scores = ({
   return (
     <div className="flex justify-between items-center dark:text-white px-2 dark:bg-slate-800 -top-9 h-8 absolute left-0 right-0">
       <div>Your Score: {playerScore}</div>
-      <div>Top Score: {topScore}</div>
+      <div className="flex items-center gap-1">
+        <AiFillTrophy
+          className="dark:text-yellow-200"
+          size={25}
+        />{" "}
+        {topScore}
+      </div>
     </div>
   );
 };
