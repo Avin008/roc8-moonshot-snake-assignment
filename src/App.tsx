b97b1362862ae3cb4a0c2e63cb944b751cd4683d
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-15 19:07:26 +0530

    feat: toogle dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { GameBoard, Navbar } from "./components";
 import { MdDarkMode } from "react-icons/md";
+import { useState } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] =
+    useState<boolean>(true);
+
+  const darkModeSetter = (): void => {
+    setIsDarkMode((prev) => !prev);
+  };
+
   return (
-    <div className="h-screen dark">
+    <div className={`h-screen ${isDarkMode && "dark"}`}>
       <div className="dark:bg-slate-800 w-full h-full flex items-center justify-center">
-        <Navbar />
+        <Navbar
+          darkMode={isDarkMode}
+          darkModeSetter={darkModeSetter}
+        />
         {/* <GameBoard /> */}
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,27 @@
-import { MdDarkMode } from "react-icons/md";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 
-const Navbar = () => {
+const Navbar = ({
+  darkMode,
+  darkModeSetter,
+}: {
+  darkMode: boolean;
+  darkModeSetter: () => void;
+}) => {
   return (
     <div className="h-14 dark:bg-slate-900 justify-between border-b absolute shadow-sm flex items-center px-8 left-0 right-0 top-0 border-gray-600">
-      <span className="font-medium text-white text-3xl">
+      <span className="font-medium dark:text-white text-3xl">
         Snakes
       </span>
       <ul>
-        <li className="text-white cursor-pointer hover:bg-slate-700 p-1 rounded-md">
-          <MdDarkMode size={25} />
+        <li
+          className="dark:text-white cursor-pointer dark:hover:bg-slate-700 p-1 rounded-md"
+          onClick={darkModeSetter}
+        >
+          {darkMode ? (
+            <MdDarkMode size={25} />
+          ) : (
+            <MdLightMode size={25} />
+          )}
         </li>
       </ul>
     </div>
