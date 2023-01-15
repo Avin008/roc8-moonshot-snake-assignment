3beba520657b12206843331d6459e73f52497841
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-15 19:30:26 +0530

    style: remove padding x

--- src/components/MobileControls.tsx
+++ src/components/MobileControls.tsx
@@ -7,7 +7,7 @@ import {
 
 const MobileControls = () => {
   return (
-    <ul className="fixed border-t shadow-md dark:border-gray-600 dark:text-white bottom-0 items-center flex justify-around left-0 right-0 px-4 dark:bg-slate-900 h-14">
+    <ul className="fixed border-t shadow-md dark:border-gray-600 dark:text-white bottom-0 items-center flex justify-around left-0 right-0 dark:bg-slate-900 h-14">
       <li>
         <MdArrowUpward size={25} />
       </li>
