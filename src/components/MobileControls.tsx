533d748ba872b95801615cba1a63abfbb828bc4e
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2023-01-15 22:59:07 +0530

    style: mobile controls on mobile size

--- src/components/MobileControls.tsx
+++ src/components/MobileControls.tsx
@@ -7,7 +7,7 @@ import {
 
 const MobileControls = () => {
   return (
-    <ul className="fixed border-t shadow-md dark:border-gray-600 dark:text-white bottom-0 items-center flex justify-around left-0 right-0 dark:bg-slate-900 h-14">
+    <ul className="fixed sm:visible lg:hidden border-t shadow-md dark:border-gray-600 dark:text-white bottom-0 items-center flex justify-around left-0 right-0 dark:bg-slate-900 h-14">
       <li className="hover:cursor-pointer dark:hover:bg-slate-800 hover:bg-gray-200 dark:active:bg-slate-600 active:bg-gray-300 p-2 rounded-full">
         <MdArrowUpward size={25} />
       </li>
